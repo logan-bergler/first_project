83959092e1bc4cbcb3f379a4247e51a7cec41adf
logan-bergler <berglerl@msu.edu>
2023-09-26 09:59:03 -0700

    Python Fundamentals Unit

---
+++ python_tests.py
@@ -0,0 +1,14 @@
+weight_kg = 60.3
+patient_id = '001'
+
+weight_lb = 2.2 * weight_kg
+patient_id = 'inflam_' + patient_id
+
+print(patient_id, 'weight in kilograms:', weight_kg)
+print(type(60.3))
+print(type(patient_id))
+print('weight in pounds:', 2.2 * weight_kg)
+print(weight_kg)
+
+weight_kg = 65.0
+print('weight in kilograms is now:', weight_kg)
